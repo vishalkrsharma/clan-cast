fb74761c3768d04bc1f05572f477d417e005f0c8
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-02-08 16:16:11 +0530

    feat: Authentication and DB setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from 'react';
+
+const AuthLayout = ({ children }: PropsWithChildren) => {
+  return <div className='min-h-screen flex items-center justify-center'>{children}</div>;
+};
+
+export default AuthLayout;
