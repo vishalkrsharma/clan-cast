439db8a0949bde494c7f59c79eaa87c7b70cfa51
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-03-10 15:00:16 +0530

    feat: add chat header

--- app/(root)/(routes)/servers/[serverId]/layout.tsx
+++ app/(root)/(routes)/servers/[serverId]/layout.tsx
@@ -33,7 +33,7 @@ const ServerIdLayout = async ({
 
   return (
     <div className='h-full'>
-      <div className='hiddenn md:flex h-full w-60 z-20 flex-col fixed inset-y-0'>
+      <div className='hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0'>
         <ServerSidebar serverId={params.serverId} />
       </div>
       <main className='h-full md:pl-60'>{children}</main>
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,17 +1,21 @@
-import { redirect } from 'next/navigation';
 import { ChannelType, MemberRole } from '@prisma/client';
+import { redirect } from 'next/navigation';
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from 'lucide-react';
 
-import db from '@/lib/db';
-import currentProfile from '@/lib/current-profile';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
+import currentProfile from '@/lib/current-profile';
+import db from '@/lib/db';
 
 import ServerHeader from './server-header';
 import ServerSearch from './server-search';
 import ServerSection from './server-section';
 import ServerChannel from './server-channel';
 import ServerMember from './server-member';
+
+interface ServerSidebarProps {
+  serverId: string;
+}
 
 const iconMap = {
   [ChannelType.TEXT]: <Hash className='mr-2 h-4 w-4' />,
@@ -25,10 +29,12 @@ const roleIconMap = {
   [MemberRole.ADMIN]: <ShieldAlert className='h-4 w-4 mr-2 text-rose-500' />,
 };
 
-const ServerSidebar = async ({ serverId }: { serverId: string }) => {
+const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   const profile = await currentProfile();
 
-  if (!profile) return redirect('/');
+  if (!profile) {
+    return redirect('/');
+  }
 
   const server = await db.server.findUnique({
     where: {
@@ -51,18 +57,19 @@ const ServerSidebar = async ({ serverId }: { serverId: string }) => {
     },
   });
 
-  if (!server) return redirect('/');
+  const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT);
+  const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO);
+  const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO);
+  const members = server?.members.filter((member) => member.profileId !== profile.id);
 
-  const textChannels = server.channels.filter((channel) => channel.type === ChannelType.TEXT);
-  const audioChannels = server.channels.filter((channel) => channel.type === ChannelType.AUDIO);
-  const videoChannels = server.channels.filter((channel) => channel.type === ChannelType.VIDEO);
-
-  const members = server.members.filter((member) => member.profileId !== profile.id);
+  if (!server) {
+    return redirect('/');
+  }
 
   const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
   return (
-    <div className='flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F2F5]'>
+    <div className='flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]'>
       <ServerHeader
         server={server}
         role={role}
